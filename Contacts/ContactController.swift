87f06b36e406fadb1acbb3b8d5f4b52f58c1b2fe
Jason Goodney <jason.goodney@gmail.com>
2018-09-28 12:06:32 -0600

    main app finished

--- Contacts/ContactController.swift
+++ Contacts/ContactController.swift
@@ -16,7 +16,7 @@ class ContactController {
     
     let contactsWereUpdatedNotification = Notification.Name("ContactsWereUpdated")
     
-    let pubDB = CKContainer.default().publicCloudDatabase
+    let privateDB = CKContainer.default().privateCloudDatabase
     
     var contacts: [Contact] = [] {
         didSet {
@@ -26,7 +26,6 @@ class ContactController {
         }
     }
     
-    
     // MARK: - Save
     func addContact(name: String, email: String, phone: String, completion: @escaping (Contact) -> Void) {
         
@@ -34,7 +33,7 @@ class ContactController {
         
         let record = CKRecord(contact: contact)
         
-        pubDB.save(record) { (record, error) in
+        privateDB.save(record) { (record, error) in
             if let error = error {
                 print("🐙Error in function: \(#function) \(error) \(error.localizedDescription)")
                 return
@@ -42,8 +41,61 @@ class ContactController {
             
             guard let record = record else { return }
             guard let contact = Contact(record: record) else { return }
+            if let index = self.contacts.firstIndex(where: { $0.name > contact.name }) {
+                self.contacts.insert(contact, at: index)
+            }
             completion(contact)
         }
     }
+    
+    // MARK: - Fetch
+    func fetchContacts(completion: @escaping ([Contact]?) -> Void) {
+        
+        let predicate = NSPredicate(value: true)
+        
+        let query = CKQuery(recordType: ContactKey.RecordType, predicate: predicate)
+        
+        query.sortDescriptors = [NSSortDescriptor(key: "Name", ascending: true)]
+        
+        privateDB.perform(query, inZoneWith: nil) { (records, error) in
+            if let error = error {
+                print("🐙Error in function: \(#function) \(error) \(error.localizedDescription)")
+                completion(nil)
+                return
+            }
+            
+            guard let records = records else {
+                completion(nil)
+                return
+            }
+            
+            self.contacts = records.compactMap { Contact(record: $0) }
+            
+            completion(self.contacts)
+        }
+    }
+    
+    // MARK: - Update
+    func updateContact(_ contact: Contact, name: String, phone: String, email: String,
+                       completion: @escaping (Bool) -> Void) {
+        
+        contact.name = name
+        contact.phone = phone
+        contact.email = email
+        
+        let record = CKRecord(contact: contact)
+        
+        let operation = CKModifyRecordsOperation(recordsToSave: [record], recordIDsToDelete: nil)
+        operation.savePolicy = .changedKeys
+        operation.queuePriority = .high
+        operation.qualityOfService = .userInitiated
+        operation.completionBlock = {
+            completion(true)
+        }
+        
+        privateDB.add(operation)
+    }
+    
+    
     
 }
--- Contacts/ContactsListViewController.swift
+++ Contacts/ContactsListViewController.swift
@@ -8,23 +8,132 @@
 
 import UIKit
 
-class ContactsListViewController: UIViewController {
-
+class ContactListViewController: UIViewController {
+    
+    // MARK: - Properties
+    let cellId = String(describing: UITableViewCell.self)
+    
+    
+    
+    // MARK: - Subviews
+    lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.dataSource = self
+        view.delegate = self
+        view.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        return view
+    }()
+    
+    lazy var addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+    
+    // MARK: - Lifcycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        updateView()
+        
+        performFetch()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadContacts), name: ContactController.shared.contactsWereUpdatedNotification, object: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        title = "Contacts"
+        
+        self.tableView.reloadData()
     }
-    */
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        title = "idk these peoeple"
+    }
+}
+
+// MARK: - UpdateView
+private extension ContactListViewController {
+    func updateView() {
+        addSubviews(subviews: [tableView])
+        setupConstraints()
+        setupNavigationBar()
+    }
+    
+    func addSubviews(subviews: [UIView]) {
+        subviews.forEach{ view.addSubview($0) }
+    }
+    
+    func setupConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    func setupNavigationBar() {
+
+        navigationItem.rightBarButtonItem = addButton
+        
+    }
 
 }
+
+// MARK: - Methods
+private extension ContactListViewController {
+    @objc func reloadContacts() {
+        
+        self.tableView.reloadData()
+    }
+    
+    func performFetch() {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        
+        ContactController.shared.fetchContacts { (contacts) in
+            guard let contacts = contacts else { return }
+            ContactController.shared.contacts = contacts
+            
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                self.reloadContacts()
+            }
+        }
+    }
+}
+
+// MARK: - User Interaction
+private extension ContactListViewController {
+    
+    @objc func addButtonTapped() {
+        let contactDetailVC = ContactDetailViewController()
+        navigationController?.pushViewController(contactDetailVC, animated: true)
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension ContactListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return ContactController.shared.contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        let contact = ContactController.shared.contacts[indexPath.row]
+        cell.textLabel?.text = contact.name
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ContactListViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let contact = ContactController.shared.contacts[indexPath.row]
+        let contactDetailVC = ContactDetailViewController()
+        contactDetailVC.contact = contact
+        contactDetailVC.isUpdating = true
+        navigationController?.pushViewController(contactDetailVC, animated: true)
+    }
+}
+
